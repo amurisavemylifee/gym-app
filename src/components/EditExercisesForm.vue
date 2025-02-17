9e6fdbb7fa63a19c0eb2fe5fecc8eac671b1a7c4
watashi812 <potokenjoyer@gmail.com>
2025-02-17 17:32:47 +0300

    add edit

---
+++ src/components/EditExercisesForm.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import type { Exercise } from '@/types';
+import { ref } from 'vue';
+
+const props = defineProps<{
+  exercise: Exercise;
+}>();
+
+const $emits = defineEmits<{
+  confirm: [data: Exercise];
+  cancel: [];
+}>();
+
+const form = ref<Exercise>({
+  exerciseId: props.exercise.exerciseId,
+  name: props.exercise.name,
+  description: props.exercise.description,
+});
+
+function onSave() {
+  $emits('confirm', form.value);
+}
+
+function onCancel() {
+  $emits('cancel');
+}
+</script>
+
+<template>
+  <div class="w-full flex flex-col gap-4 justify-center">
+    <InputText v-model="form.name" type="text" placeholder="Название" />
+    <InputText v-model="form.description" type="text" placeholder="Описание" />
+    <div class="flex justify-between mt-10">
+      <Button severity="success" @click="onSave">Сохранить</Button>
+      <Button severity="danger" @click="onCancel">Отмена</Button>
+    </div>
+  </div>
+</template>
--- src/pages/PageExercises.vue
+++ src/pages/PageExercises.vue
@@ -1,15 +1,32 @@
 <script setup lang="ts">
-import type { CreateExercise } from '@/types';
-
+import type { Exercise } from '@/types';
 import CreateExercisesForm from '@/components/CreateExercisesForm.vue';
+import EditExercisesForm from '@/components/EditExercisesForm.vue';
 
 const exercisesStore = useExercisesStore();
-
 const isCreateDialogVisible = ref(false);
+const isEditDialogVisible = ref(false);
+const selectedExercise = ref<Exercise | null>(null);
 
-function onConfirm(data: CreateExercise) {
+function onConfirmCreate(data: Exercise) {
   exercisesStore.addExercise(data);
   isCreateDialogVisible.value = false;
+}
+
+function onConfirmEdit(data: Exercise) {
+  exercisesStore.updateExercise(data.exerciseId, data);
+  isEditDialogVisible.value = false;
+  selectedExercise.value = null;
+}
+
+function onEdit(exercise: Exercise) {
+  selectedExercise.value = exercise;
+  isEditDialogVisible.value = true;
+}
+
+function onCancelEdit() {
+  isEditDialogVisible.value = false;
+  selectedExercise.value = null;
 }
 </script>
 
@@ -18,45 +35,36 @@ function onConfirm(data: CreateExercise) {
     <template v-if="exercisesStore.exercises.length">
       <DataTable :value="exercisesStore.exercises">
         <div class="flex justify-end">
-        <Button
-          severity="success"
-          @click="isCreateDialogVisible = true">
-          Добавить
-        </Button>
-      </div>
-        <Column
-          field="name"
-          header="Название" />
-        <Column
-          field="description"
-          header="Описание" />
+          <Button severity="success" @click="isCreateDialogVisible = true">Добавить</Button>
+        </div>
+        <Column field="name" header="Название" />
+        <Column field="description" header="Описание" />
         <Column>
           <template #body="{ data }">
-            <Button
-              severity="danger"
-              icon="pi pi-trash"
-              @click="exercisesStore.removeExercise(data.exerciseId)" />
+            <div class="flex gap-2">
+              <Button severity="warn" icon="pi pi-pencil" @click="onEdit(data)" />
+              <Button severity="danger" icon="pi pi-trash" @click="exercisesStore.removeExercise(data.exerciseId)" />
+            </div>
           </template>
         </Column>
-
       </DataTable>
     </template>
-
     <template v-else>
       <div class="text-3xl h-full flex flex-col gap-4 items-center justify-center">
-        <span>Упражнения не найдены</span>
-        <Button
-          severity="success"
-          @click="isCreateDialogVisible = true">
-          Добавить
-        </Button>
+        <span>Упражнения не найдены</span>
+        <Button severity="success" @click="isCreateDialogVisible = true">Добавить</Button>
       </div>
     </template>
-    <Dialog
-      log
-      v-model:visible="isCreateDialogVisible"
-      modal>
-      <CreateExercisesForm @confirm="onConfirm" />
+    <Dialog v-model:visible="isCreateDialogVisible" modal>
+      <CreateExercisesForm @confirm="onConfirmCreate" />
+    </Dialog>
+    <Dialog v-model:visible="isEditDialogVisible" modal>
+      <EditExercisesForm
+        v-if="selectedExercise"
+        :exercise="selectedExercise"
+        @confirm="onConfirmEdit"
+        @cancel="onCancelEdit"
+      />
     </Dialog>
   </div>
 </template>
