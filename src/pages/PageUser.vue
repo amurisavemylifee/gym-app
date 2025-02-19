6f5b8da1a835d7e003a32b7fb70953f0e78d7afa
watashi812 <potokenjoyer@gmail.com>
2025-02-19 16:18:22 +0300

    add PageUser

---
+++ src/pages/PageUser.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+<div class="text-3xl h-full flex flex-col gap-4 items-center ">
+<Span>
+    Профиль
+</Span>
+<span>
+    123
+</span>
+
+</div>
+</template>
